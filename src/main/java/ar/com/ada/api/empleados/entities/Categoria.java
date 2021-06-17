33bf3f045cad73ed2155bcd53e9f2f8b1ea34d15
Muriel3000 <murielmartinacorrea@gmail.com>
2021-06-17 19:26:18 -0300

    Se completa codigo para correr el programa

--- src/main/java/ar/com/ada/api/empleados/entities/Categoria.java
+++ src/main/java/ar/com/ada/api/empleados/entities/Categoria.java
@@ -1,5 +1,63 @@
 package ar.com.ada.api.empleados.entities;
 
+import java.math.BigDecimal;
+import java.util.*;
+import javax.persistence.*;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+@Table(name = "categoria")
 public class Categoria {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "categoria_id")
+    private Integer categoriaId;
+
+    private String nombre;
+    
+    @Column(name = "sueldo_base")
+    private BigDecimal sueldoBase;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "categoria", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private List<Empleado> empleados = new ArrayList<>();
+
+    public Integer getCategoriaId() {
+        return categoriaId;
+    }
+
+    public void setCategoriaId(Integer categoriaId) {
+        this.categoriaId = categoriaId;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public BigDecimal getSueldoBase() {
+        return sueldoBase;
+    }
+
+    public void setSueldoBase(BigDecimal sueldoBase) {
+        this.sueldoBase = sueldoBase;
+    }
+
+    public List<Empleado> getEmpleados() {
+        return empleados;
+    }
+
+    public void setEmpleados(List<Empleado> empleados) {
+        this.empleados = empleados;
+    }
+
+    public void agregarEmpleado(Empleado empleado){
+        this.empleados.add(empleado);
+    }
+
 }
--- src/main/java/ar/com/ada/api/empleados/services/CategoriaService.java
+++ src/main/java/ar/com/ada/api/empleados/services/CategoriaService.java
@@ -1,5 +1,23 @@
 package ar.com.ada.api.empleados.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import ar.com.ada.api.empleados.entities.Categoria;
+import ar.com.ada.api.empleados.repos.CategoriaRepository;
+import java.util.*;
+
+@Service
 public class CategoriaService {
+
+    @Autowired
+    CategoriaRepository repository;
+
+    public void crearCategoria(Categoria categoria){
+        repository.save(categoria);
+    }
     
+    public List<Categoria> traerCategorias(){
+        return repository.findAll();
+    }
 }
